a6f295e07727584fd90fd09712a6d8b67ec62afb
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-03-14 06:56:52 +0545

    doc formatted

--- Basics/D5-6-string.py
+++ Basics/D5-6-string.py
@@ -2,7 +2,7 @@
 # print(str[-1])  #last char
 # print(str[::-1]) #reverse string
 
-#slicing
+# slicing
 
 # print(str[1:5]) #1st and 5th included -> aksh
 # print(str[0:5]) #same thing, leaves last character
@@ -15,7 +15,7 @@
 # print("Escape sequencing with python \" hi I am Sakshi \" ")
 # print('''or I can simply do 'this' ''')
 
-#SECTION - index() method -> index(substring, begp, endp)
+# SECTION - index() method -> index(substring, begp, endp)
 
 # str = 'sakshishshsh is sakshi'
 # print(str.index('sh')) # returns the index of first index of substring, by default begp - 0, endp - length of string
@@ -25,14 +25,14 @@
 # print(str.index('sh')) #first index -> 3
 # print(str.rindex('sh')) #highest index -> 10
 
-#SECTION - case changing of Strings
+# SECTION - case changing of Strings
 
 # print(str.lower()) #lower case
 # print(str.upper()) #upper case
 # print(str.title()) #title case
 
-#SECTION - string built-in methods
-string1 = 'sakshi is sakshi, she is sweet'
+# SECTION - string built-in methods
+string1 = "sakshi is sakshi, she is sweet"
 
-print(string1.startswith('sh')) #false
-print(string1.endswith('et')) #true
+print(string1.startswith("sh"))  # false
+print(string1.endswith("et"))  # true
