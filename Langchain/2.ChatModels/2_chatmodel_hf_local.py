fb4f31fc80aafee2db5cce25f5d62c0e36d676ce
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-08-31 15:55:46 +0545

    chatmodels practice, ref. CampusX tutorial

---
+++ Langchain/2.ChatModels/2_chatmodel_hf_local.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='mistralai/Mistral-7B-Instruct-v0.3',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.8,
+        max_new_tokens=150
+    )
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of Nepal?")
+print(result.content)
