ca5b06f172bd0c8a50234e680908d7a0b12c6bf6
Sakshi Khatiwada <sakshikhatiwada37@gmail.com>
2025-10-09 21:02:27 +0545

    tested embedding models

--- Langchain/3.EmbeddingModels/embedding.py
+++ Langchain/3.EmbeddingModels/embedding.py
@@ -4,10 +4,19 @@ from langchain.schema import Document
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-# embedding= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2") 
+# embedding= HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2") # works
+# print(embedding_model.embed_query("test"))'
+
+# embedding = HuggingFaceEmbeddings(model_name="google/embeddinggemma-300m") 
 # print(embedding_model.embed_query("test"))
-embedding = HuggingFaceEmbeddings(model_name="google/embeddinggemma-300m") 
-# print(embedding_model.embed_query("test"))
+
+# embedding = HuggingFaceEmbeddings(model_name="KaLM-Embedding/KaLM-embedding-multilingual-mini-instruct-v2.5")  # [[0.52445089 0.59335902 0.54547467 0.57080437 0.7015721 ]]
+
+# embedding = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-8B") # too much time taken
+# embedding2 = HuggingFaceEmbeddings(model_name="jinaai/jina-embeddings-v4") 
+
+# embedding = HuggingFaceEmbeddings(model_name="ai-sage/Giga-Embeddings-instruct") 
+# embedding = HuggingFaceEmbeddings(model_name="tencent/Youtu-Embedding",model_kwargs={"trust_remote_code": True}) # allowing to use in our model, be careful before trusting 
 
 
 document = [
@@ -22,5 +31,7 @@ query = 'Tell me about Sakshi'
 
 doc_embeddings = embedding.embed_documents(document)
 query_embeddings = embedding.embed_query(query)
-
 print(cosine_similarity([query_embeddings], doc_embeddings))
+
+# doc_embeddings2 = embedding2.embed_documents(document)
+# query_embeddings2 = embedding2.embed_query(query)
